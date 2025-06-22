b51c3214a91cb05ba08058550419d92bc020ee9b
taufik <taufika.ug20.cse@nitp.ac.in>
2025-06-22 15:17:50 +0530

    added the constructor and getter setter in User class

--- src/main/java/com/parspecassignment/urlshortner/entity/User.java
+++ src/main/java/com/parspecassignment/urlshortner/entity/User.java
@@ -17,4 +17,26 @@ public class User {
     @Column(unique = true)
     private String username;
     private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
 }
